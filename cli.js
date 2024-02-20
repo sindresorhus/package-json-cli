c674afce8a286d66acfc7ea16a640a3fea658541
tommy-mitchell <tmitchell7@uh.edu>
2024-02-20 15:03:09 -0600

    Target Node.js 18, update dependencies

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import process from 'node:process';
 import meow from 'meow';
-import filterObject from 'filter-obj';
+import {includeKeys} from 'filter-obj';
 import packageJson from 'package-json';
 
 const cli = meow(`
@@ -12,7 +12,7 @@ const cli = meow(`
 	  $ package-json ava
 	  {
 	    "name": "ava",
-	    "version": "0.18.0",
+	    "version": "6.1.1",
 	    …
 	  }
 `, {
@@ -27,6 +27,6 @@ if (!packageName) {
 }
 
 let package_ = await packageJson(packageName, {version});
-package_ = filterObject(package_, key => key[0] !== '_' && key !== 'directories');
+package_ = includeKeys(package_, key => key.at(0) !== '_' && key !== 'directories');
 
 console.log(JSON.stringify(package_, undefined, '  '));
