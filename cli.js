8c88ea5bef43922c759c41987bde4660af91414a
tommy-mitchell <tmitchell7@uh.edu>
2024-02-21 01:39:04 -0600

    use more ergonomic object filtering

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import process from 'node:process';
 import meow from 'meow';
-import {includeKeys} from 'filter-obj';
+import {excludeKeys} from 'filter-obj';
 import packageJson from 'package-json';
 
 const cli = meow(`
@@ -27,6 +27,6 @@ if (!packageName) {
 }
 
 let package_ = await packageJson(packageName, {version});
-package_ = includeKeys(package_, key => key.at(0) !== '_' && key !== 'directories');
+package_ = excludeKeys(package_, key => key.startsWith('_') || key === 'directories');
 
 console.log(JSON.stringify(package_, undefined, '  '));
